099e698740232a6e308676f652a0aa42091f5088
reh13597 <alexlguo18@gmail.com>
2025-04-16 20:11:02 -0700

    Tweaked the navbar for phones

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,8 +1,10 @@
 <div class="navbar h-20 bg-base-100 shadow-sm">
     <div class="navbar-start">
       <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-neutral btn-ghost btn-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+        <div tabindex="0">
+          <a class="btn btn-neutral btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+          </a>
         </div>
         <ul
           tabindex="0"
@@ -14,10 +16,10 @@
       </div>
     </div>
     <div class="navbar-center">
-      <a class="font-mono btn btn-neutral btn-ghost text-2xl">&#x1F517 SERVER NEXUS &#x2699</a>
+      <a class="font-mono btn btn-neutral btn-ghost text-2xl" href="#/">&#x1F517 SERVER NEXUS &#x2699</a>
     </div>
     <div class="navbar-end">
-        <a class="btn btn-neutral btn-ghost btn-circle">
+        <a class="btn btn-neutral btn-ghost">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 12c2.67 0 8 1.34 8 4v2H4v-2c0-2.66 5.33-4 8-4zm0-2a4 4 0 100-8 4 4 0 000 8z" /> </svg>
         </a>
       </div>
