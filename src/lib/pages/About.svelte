0a3d929dcdc4b1e5b8ccf3ed04192ac7f85d8284
reh13597 <alexlguo18@gmail.com>
2026-01-22 00:20:39 -0800

    Added content to About & Contact pages

--- src/lib/pages/About.svelte
+++ src/lib/pages/About.svelte
@@ -1,3 +1,9 @@
-<div>
-    <h1 class="text-4xl font-bold mt-10">About Page</h1>
+<div class="px-4 max-w-2xl mx-auto">
+    <h1 class="text-6xl font-bold mt-10 text-primary">About</h1>
+    <p class="text-xl mt-10 text-balance">I (Alex) built this website with the purpose of addressing some issues I often ran into when dealing with information about Minecraft servers.</p>
+    <p class="text-xl mt-10 text-balance">1. Most websites that had Minecraft server information were not very intuitive or reliable.</p>
+    <p class="text-xl mt-10 text-balance">2. There were no websites where people could save their favorite servers or advertise their own.</p>
+    <p class="text-xl mt-10 text-balance">3. There were also no websites that offered server world maps, player stats & achievements, and live server chat feeds, all in one place.</p>
+    <p class="text-xl mt-10 text-balance">Because of these 3 main issues, I decided to take matters into my own hands and here we are now! As an avid Minecraft fan and programmer, I wanted to express my passion for it in code. This website is still a work in progress, so there will be much more to come!</p>
+    <p class="text-xl mt-10 text-balance">Have fun and enjoy!</p>
 </div>
--- src/lib/pages/Contact.svelte
+++ src/lib/pages/Contact.svelte
@@ -1,3 +1,5 @@
-<div>
-    <h1 class="text-4xl font-bold mt-10">Contact Page</h1>
+<div class="px-4">
+    <h1 class="text-6xl font-bold mt-10 text-primary">Contact</h1>
+    <p class="text-xl mt-10">Feel free to contact me: alg17@sfu.ca</p>
+    <p class="text-xl mt-10">More content coming soon...</p>
 </div>
