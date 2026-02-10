8f5bcfc9e47c891287556a18a53a78b01cf11809
reh13597 <alexlguo18@gmail.com>
2026-02-09 20:03:30 -0800

    updated about page

--- src/lib/pages/About.svelte
+++ src/lib/pages/About.svelte
@@ -1,9 +1,10 @@
 <div class="px-4 max-w-2xl mx-auto pb-10 select-none">
     <h1 class="text-6xl font-bold mt-30 text-primary">About</h1>
-    <p class="text-xl mt-10 text-balance">I (Alex) built this website with the purpose of addressing some issues I often run into when dealing with information about Minecraft servers.</p>
+    <p class="text-xl mt-10 text-balance">I built this website with the purpose of addressing some issues I often run into when dealing with information about Minecraft servers.</p>
     <p class="text-xl mt-10 text-balance">1. Most websites that have Minecraft server information are not very intuitive or reliable.</p>
-    <p class="text-xl mt-10 text-balance">2. There aren't many websites where people can save their favorite servers or rate them.</p>
-    <p class="text-xl mt-10 text-balance">3. There also aren't many websites that offer the option to see other people's profiles, with their favorite servers and ratings listed.</p>
+    <p class="text-xl mt-10 text-balance">2. There aren't many websites where people can save their favorite servers or leave reviews/ratings on them.</p>
+    <p class="text-xl mt-10 text-balance">3. There also aren't many websites that offer the option to see other people's profiles, with their favorite servers and reviews/ratings listed.</p>
     <p class="text-xl mt-10 text-balance">Because of these 3 main issues, I decided to take matters into my own hands and here we are now! As an avid Minecraft fan and programmer, I wanted to express my passion for it in code. This website is still a work in progress, so there will be much more to come!</p>
     <p class="text-xl mt-10 text-balance">Have fun and enjoy!</p>
+    <p class="text-xl mt-10 text-balance text-primary">Note: This website is still a work in progress, so there will be much more to come!</p>
 </div>
